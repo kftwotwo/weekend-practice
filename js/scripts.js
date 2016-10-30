b620297b56ad2d715163287165aa141d82081fd2
Kevin A. Finley <kf.two.two@gmail.com>
2016-10-30 12:06:15 -0700

    add jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
+$(document).ready(function() {
+  $("#input").submit(function(event) {
+    var nameSpan = $("input#name").val();
+    var phoneSpan = $("input#phone").val();
+    var emailSpan = $("input#email").val();
+    var subjectSpan = $("input#subject").val();
+    var messageSpan = $("input#message").val();
 
+    $(".name").text(nameSpan);
+    $(".phone").text(phoneSpan);
+    $(".email").text(emailSpan);
+    $(".subject").text(subjectSpan);
+    $(".message").text(messageSpan);
 
-
-
-
-$(document).ready(function(){
-  $("form#input").submit(function(event){
-    event.preventDeafult();
-
-
-    $("form#input").hide();
+    $("#inputForm").hide();
+    $("#outputForm").show();
   });
+  event.preventDeafult();
 });
